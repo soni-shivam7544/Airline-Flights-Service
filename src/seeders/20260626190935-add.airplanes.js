9cf6541cc896c136b696fc17992ed3befb8f7b5f
soni-shivam7544 <sonishivam7544@gmail.com>
2026-06-28 23:51:29 +0530

    Created Seeder file for airplanes

---
+++ src/seeders/20260626190935-add.airplanes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const {Op} = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airplanes', [
+      {
+        modelNumber: 'airbus541',
+        capacity: 900,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'boeing555',
+        capacity: 504,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Airplanes', {[Op.or]: [{modelNumber: 'airbus541'}, {modelNumber: 'boeing555'}]});
+  }
+};
